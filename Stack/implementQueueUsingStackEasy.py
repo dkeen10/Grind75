5d88b1129d9c46e361de57e2eea2f09d7cbf5379
Duncan Keen <duncankeen@gmail.com>
2024-01-30 21:20:35 -0800

    Create implementQueueUsingStackEasy.py

---
+++ Stack/implementQueueUsingStackEasy.py
@@ -0,0 +1,79 @@
+# https://leetcode.com/problems/implement-queue-using-stacks/description/
+
+"""
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+    void push(int x) Pushes element x to the back of the queue.
+    int pop() Removes the element from the front of the queue and returns it.
+    int peek() Returns the element at the front of the queue.
+    boolean empty() Returns true if the queue is empty, false otherwise.
+
+Notes:
+
+    You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+    Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+ 
+
+Example 1:
+
+Input
+["MyQueue", "push", "push", "peek", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 1, 1, false]
+
+Explanation
+MyQueue myQueue = new MyQueue();
+myQueue.push(1); // queue is: [1]
+myQueue.push(2); // queue is: [1, 2] (leftmost is front of the queue)
+myQueue.peek(); // return 1
+myQueue.pop(); // return 1, queue is [2]
+myQueue.empty(); // return false
+"""
+
+
+class MyQueue:
+    """
+    Add to in_queue and pop from out_queue to similate the FIFO queue.
+    """
+    def __init__(self):
+        self.in_queue = []
+        self.out_queue = []
+
+    def push(self, x: int) -> None:
+        self.in_queue.append(x)
+
+    def pop(self) -> int:
+        if not self.out_queue:
+            while self.in_queue:
+                self.out_queue.append(self.in_queue.pop())
+        return self.out_queue.pop()
+
+    def peek(self) -> int:
+        if not self.out_queue:
+            while self.in_queue:
+                self.out_queue.append(self.in_queue.pop())
+        return self.out_queue[-1]
+
+    def empty(self) -> bool:
+        return not (self.in_queue or self.out_queue)
+
+
+def main():
+    # Your MyQueue object will be instantiated and called as such:
+
+    obj = MyQueue()
+    obj.push(1)
+    obj.push(2)
+    print(obj.peek())
+    print(obj.pop())
+    print(obj.empty())
+
+
+
+
+if __name__ == '__main__':
+    main()
