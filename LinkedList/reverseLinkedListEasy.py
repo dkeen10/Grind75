2e2bc5fb62ef8c60691f0195add2625e23d158e4
Duncan Keen <duncankeen@gmail.com>
2024-02-06 20:45:57 -0800

    Update reverseLinkedListEasy.py

---
+++ LinkedList/reverseLinkedListEasy.py
@@ -0,0 +1,46 @@
+# https://leetcode.com/problems/reverse-linked-list/description/
+
+"""
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Example 2:
+
+Input: head = [1,2]
+Output: [2,1]
+
+Example 3:
+
+Input: head = []
+Output: []
+"""
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+        
+class Solution:
+    def reverseList(self, head: [ListNode]) -> [ListNode]:
+        pass
+
+
+def main():
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    print(Solution().reverseList(head))
+
+
+if __name__ == "__main__":
+    main()
+
+
+        
