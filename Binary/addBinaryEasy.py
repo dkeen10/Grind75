a65cceeaee8788bdd31fd18bcb82df6da12a86f2
Duncan Keen <duncankeen@gmail.com>
2024-02-06 21:45:26 -0800

    add binary init

---
+++ Binary/addBinaryEasy.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/add-binary/description/
+
+"""
+Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+ """
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        pass
+
+
+def main():
+    a = "11"
+    b = "1"
+    print(Solution().addBinary(a, b))
+
+    a = "1010"
+    b = "1011"
+    print(Solution().addBinary(a, b))
+
+
+
+if __name__ == "__main__":
+    main()
+
