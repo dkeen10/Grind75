4581641423cdee308bb51ad71f3a7dfc79a13225
Duncan Keen <duncankeen@gmail.com>
2024-02-09 15:42:56 -0800

    Update diameterOfBinaryTreeEasy.py

--- BinaryTree/diameterOfBinaryTreeEasy.py
+++ BinaryTree/diameterOfBinaryTreeEasy.py
@@ -32,8 +32,20 @@ class TreeNode:
 
 
 class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        pass
+    def diameterOfBinaryTree(self, root: [TreeNode]) -> int:
+        self.diameter = 0
+        self.dfs(root)
+        return self.diameter
+    
+    def dfs(self, node):
+        # base case:
+        if node == None:
+            return 0
+		# recursive cases
+        left_height = self.dfs(node.left)
+        right_height = self.dfs(node.right)
+        self.diameter = max(self.diameter,left_height + right_height )
+        return max(left_height,right_height) + 1
 
 def main():
     root = [1, 2, 3, 4, 5]
