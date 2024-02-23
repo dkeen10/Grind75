95e73ab695c3682fe8c3d3f5bfa234fd6c1af7a1
Duncan Keen <duncankeen@gmail.com>
2024-02-22 21:53:20 -0800

    Create binaryTreeTraversalMedium.py

---
+++ BinaryTree/binaryTreeTraversalMedium.py
@@ -0,0 +1,47 @@
+# https://leetcode.com/problems/binary-tree-level-order-traversal/description/
+
+
+"""
+Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+ 
+
+Example 1:
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[9,20],[15,7]]
+
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+
+Example 3:
+
+Input: root = []
+Output: []
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def levelOrder(self, root: [TreeNode]) -> list[list[int]]:
+        
+
+
+def main():
+    root = [3,9,20,None,None,15,7]
+    sol = Solution()
+    print(sol.levelOrder(root))
+
+    
+
+
+if __name__ == "__main__":
+    main()
