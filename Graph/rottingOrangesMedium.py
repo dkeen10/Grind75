9a4e55ad9722d58e1d92a5db3fe59a1a1052d321
Duncan Keen <duncankeen@gmail.com>
2024-03-12 21:13:00 -0700

    Update rottingOrangesMedium.py

--- Graph/rottingOrangesMedium.py
+++ Graph/rottingOrangesMedium.py
@@ -10,7 +10,6 @@ Every minute, any fresh orange that is 4-directionally adjacent to a rotten oran
 
 Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
 
- 
 
 Example 1:
 
@@ -29,42 +28,67 @@ Output: 0
 Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
  """
 
+from collections import deque
+
 
 class Solution:
     def orangesRotting(self, grid: list[list[int]]) -> int:
-        minTime = 0
-        freshOranges = 0
-        rottenOranges = []
-
-        # count the number of fresh oranges and store the rotten oranges in a list
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 1:
-                    freshOranges += 1
-                elif grid[i][j] == 2:
-                    rottenOranges.append((i, j))
-
-        # if there are no fresh oranges, return 0
-        if freshOranges == 0:
-            return 0
-        
-        # if there are no rotten oranges, return -1
-        if not rottenOranges:
+        rows = len(grid)
+        if rows == 0:  
             return -1
         
-        # while there are fresh oranges and there are rotten oranges
-        while freshOranges > 0 and rottenOranges:
-            newRottenOranges = []
-            for i, j in rottenOranges:
-                for x, y in [(i+1, j), (i-1, j), (i, j+1), (i, j-1)]:
-                    if 0 <= x < len(grid) and 0 <= y < len(grid[i]) and grid[x][y] == 1:
-                        grid[x][y] = 2
-                        freshOranges -= 1
-                        newRottenOranges.append((x, y))
-            rottenOranges = newRottenOranges
-            minTime += 1
+        cols = len(grid[0])
+        
+        
+        fresh_count = 0
+        
+        # queue with rotten oranges (for BFS)
+        rotten = deque()
+        
+        # visit each cell in the grid
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 2:
+                    # add the rotten orange coordinates to the queue
+                    rotten.append((row, col))
+                elif grid[row][col] == 1:
+                    # update fresh oranges count
+                    fresh_count += 1
+        
+        # keep track of minutes passed.
+        minutes_passed = 0
+        
+        # If there are rotten oranges in the queue and there are still fresh oranges in the grid keep looping
+        while rotten and fresh_count > 0:
+            minutes_passed += 1
+            
+            # process rotten oranges on the current level
+            for _ in range(len(rotten)):
+                # this is my deque is used instead of list, since popleft() doesnt realloc memory
+                x, y = rotten.popleft()
+                
+                # visit all the adjacent cells
+                for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
+                    # calculate the coordinates of the adjacent cell
+                    xx, yy = x + dx, y + dy
+                    # ignore the cell if it is out of the grid boundary
+                    if xx < 0 or xx == rows or yy < 0 or yy == cols:
+                        continue
+                    # ignore the cell if it is empty '0' or visited before '2'
+                    if grid[xx][yy] == 0 or grid[xx][yy] == 2:
+                        continue
+                        
+                    # update the fresh oranges count
+                    fresh_count -= 1
+                    
+                    # mark the current fresh orange as rotten
+                    grid[xx][yy] = 2
+                    
+                    # add the current rotten to the queue
+                    rotten.append((xx, yy))
 
-        return minTime if freshOranges > 0 else -1
+        # return -1 if there are fresh oranges left in the grid (there were no adjacent rotten oranges to make them rotten)
+        return minutes_passed if fresh_count == 0 else -1
     
 
 def main():
