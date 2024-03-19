3ff97fe9af7070b895df6d9c3b36f072994c0612
Duncan Keen <duncankeen@gmail.com>
2024-03-18 23:17:24 -0700

    Update combinationSum.py

--- Array/combinationSum.py
+++ Array/combinationSum.py
@@ -35,7 +35,19 @@ Output: []
 
 class Solution:
     def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
-        pass
+        combinations = []
+
+        def backtrack(start, target, path):
+            if target < 0:
+                return
+            if target == 0:
+                combinations.append(path)
+                return
+            for i in range(start, len(candidates)):
+                backtrack(i, target - candidates[i], path + [candidates[i]])
+
+        backtrack(0, target, [])
+        return combinations
     
 
 def main():
