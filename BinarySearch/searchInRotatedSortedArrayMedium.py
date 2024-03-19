9dab07fc2c7d8d2af852c9f6360b72a99569f791
Duncan Keen <duncankeen@gmail.com>
2024-03-18 23:13:45 -0700

    Update searchInRotatedSortedArrayMedium.py

--- BinarySearch/searchInRotatedSortedArrayMedium.py
+++ BinarySearch/searchInRotatedSortedArrayMedium.py
@@ -29,9 +29,38 @@ Output: -1
 
 
 class Solution:
-    def search(self, nums: list[int], target: int) -> int:
-        pass
+    def base_binary_search(self, nums: list[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
 
+        while start <= end:
+            mid = start + (end - start)//2
+            if nums[mid] > target:
+                end = mid - 1
+            elif nums[mid] < target:
+                start = mid + 1
+            else:
+                return mid
+        return -1
+
+
+    def base_binary_search(self, nums: list[int], target: int) -> int:
+        start = 0
+        end = len(nums) - 1
+
+        while start <= end:
+            mid = (end + start)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < nums[end]:
+                if target > nums[mid] and target <= nums[end]:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            else:
+                if target >= nums[start] and target < nums[mid]: end = mid -1
+                else : start = mid + 1
+        return -1
 def main():
     pass
 
